5db6fa29c4756a09f47d73f2c8b3ba8426b11127
Yar14k <tabachuk1406@gmail.com>
2026-03-19 10:03:18 +0100

    Add solution

--- src/components/PeoplePage/PersonLink.tsx
+++ src/components/PeoplePage/PersonLink.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { Person } from '../../types';
 
 type PersonLinkProps = {
@@ -6,9 +6,12 @@ type PersonLinkProps = {
 };
 
 const PersonLink = ({ person }: PersonLinkProps) => {
+  const location = useLocation();
+  const search = location.search;
+
   return (
     <Link
-      to={`/people/${person.slug}`}
+      to={`/people/${person.slug}${search}`}
       className={person.sex === 'f' ? 'has-text-danger' : ''}
     >
       {person.name}
